db5dfb8f13635e63ddd78939b5a8bc822705533c
Dmitriy <dmitriy.shehobalov@gmail.com>
2022-11-08 13:28:41 +0300

    add task25

---
+++ task25/Program.cs
@@ -0,0 +1,24 @@
+﻿// // Задача 25: 
+// 1.Напишите цикл, который принимает на вход два числа (A и B) и
+// 2.возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Enter first number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int RaisedToThePower(int num, int num2)
+{
+    int raised = num;
+    for (int i = 1; i < num2; i++)
+    {
+        raised = raised * num;
+    }
+    return raised;
+}
+int result = RaisedToThePower(number, number2);
+Console.WriteLine($"The number {number} raised to the power {number2} = {result}");
